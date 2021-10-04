9b9de408335d7f7d32962987425d3f94c1661833
Pratish <psnanandkar@gmail.com>
2021-10-04 23:00:14 +0530

    NA

--- src/main/java/com/first/Greet.java
+++ src/main/java/com/first/Greet.java
@@ -4,7 +4,7 @@ public class Greet {
 
 	public static void main(String[] args) {
 		for (int i = 1; i <= 10; i++) {
-			System.out.println("Helllo... " + i);
+			System.out.println("Hellllllllllo... " + i);
 		}
 	}
 }
